9df50ceb69be0b3d584ff2cb820c3d4ba8717ffe
Yves Gillet <yves.gillet@weareadaptive.com>
2021-02-12 15:56:26 -0500

    test cors

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-
-sam package --template-file target/sam.native.yaml --output-template-file packaged.yaml --s3-bucket cocosoft  --s3-prefix rateconv
-
-sam deploy --template-file packaged.yaml --capabilities CAPABILITY_IAM --stack-name rateconv
